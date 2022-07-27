07067629d866b56f2daf39fce539063679f55d7c
gunboe <guntherboeckmann@gmail.com>
2022-07-27 00:17:14 -0300

    Ajustes do cabeçalho para manter único, issue #1

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 function goHome() {
     open("index.html","_self")
 }
+
+function openSobre() {
+    document.querySelector('iframe').src="sobre.html"
+}
+function openReceitas() {
+    document.querySelector('iframe').src="receitas.html"
+}
