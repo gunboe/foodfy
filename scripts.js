87ccc074ec8f8c54e1cfc00b56bba91c0e812e9a
gunboe <guntherboeckmann@gmail.com>
2022-07-31 16:56:08 -0300

    Desafio 1 completo com Modal e conteudo por card.

--- scripts.js
+++ scripts.js
@@ -8,11 +8,16 @@ const cards = document.querySelectorAll(".card")
 for (let card of cards) {
     card.addEventListener("click",function(){
         modalOverlay.classList.add("active")
-        const id_img = card.getAttribute("id")
-        document.querySelector(".modal_img").src=`../layouts/assets/${id_img}.png`
+        var id_receita = card.getAttribute("id")
+        document.querySelector(".modal_img").src=`../layouts/assets/${id_receita}.png`
+        retorna_texto(id_receita)
     })
 }
 
+function retorna_texto(id_receita) {
+    var receita = `<b>Receita de ${id_receita}</b><br>fhaeui sdufhuis hf orfhsf blkzkdvnld osadhsidf;sdihfosafuh woeuhfsdbfdsfhd.</br>`
+    document.getElementById('card_content').innerHTML = receita
+}
 
 function fecha_modal() {
     modalOverlay.classList.remove("active")
