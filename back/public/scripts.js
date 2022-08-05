12e1c11ac9d018d49dad08b65689310952752d71
gunboe <guntherboeckmann@gmail.com>
2022-08-05 01:40:17 -0300

    Configuração de Estralas e ajustes gerais

--- back/public/scripts.js
+++ back/public/scripts.js
@@ -6,18 +6,26 @@ const modalOverlay = document.querySelector(".modal_overlay")
 const cards = document.querySelectorAll(".card")
 
 for (let card of cards) {
+    var stars = card.getAttribute("stars")
+    var id_receita = card.getAttribute("id")
+    var receita = card.getAttribute("receita")
+
+    // Atribuiçao de Stars
+    document.getElementById(`stars_${id_receita}`).innerHTML = stars+'<span class="material-symbols-outlined">star</span>'
+
+    // Criação de eventos para o Modal
     card.addEventListener("click",function(){
         modalOverlay.classList.add("active")
+        var stars = card.getAttribute("stars")
         var id_receita = card.getAttribute("id")
-        console.log(`${id_receita}`)
-        var receita = card.getAttribute(`receita`)
-        // console.log(receita)
+        var receita = card.getAttribute("receita")
+    
         document.querySelector(".modal_img").src=`/${id_receita}.png`
-        retorna_texto(id_receita,receita)
+        retorna_texto(id_receita,receita,stars)
     })
 }
-
-function retorna_texto(id_receita,receita) {
+ 
+function retorna_texto(id_receita,receita,stars) {
     // Conteudo estático
     // var receita = `<b>Receita de ${id_receita}</b><br>fhaeui sdufhuis hf orfhsf blkzkdvnld osadhsidf;sdihfosafuh woeuhfsdbfdsfhd.</br>`
     // document.getElementById('card_content').innerHTML = receita
@@ -27,6 +35,7 @@ function retorna_texto(id_receita,receita) {
     //var listaReceitas = JSON.parse
     var content_receita = `<b>Receita de ${id_receita}</b><br>`+receita
     document.getElementById('card_content').innerHTML = content_receita
+    document.getElementById('stars_modal').innerHTML = stars+'<span class="material-symbols-outlined">star</span>'
 }
 
 function fecha_modal() {
--- back/server.js
+++ back/server.js
@@ -9,13 +9,14 @@ server.listen(5000, console.log("Server running..."))
 
 // Inicialização do Template Engine e suas Views (Rotas)
 nunjucks.configure("views",{
-    express: server
+    express: server,
+    autoescape: false
 })
 
-// Rotas(Views)
-server.get("/", (req,res,) => res.render("index", {items: cards}))
-server.get("/receitas", (req,res,) => res.render("receitas", {items: cards}))
-server.get("/sobre", (req,res,) => res.render("sobre"))
+// Rotas(Views extendidas com nunjucks) tratam REQueries e RESponses com o método "render(ROTA,[OBJS])"
+server.get("/", (req,res,) => res.render("index", {items: cards} ))
+server.get("/receitas", (req,res,) => res.render("receitas", {items: cards} ))
+server.get("/sobre", (req, res) => res.render("sobre", {dados: "<br>Outros..."} ))
 
 // Definição das subpastas para o Template Engine
 server.set("view engine","njk")
